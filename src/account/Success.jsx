7d393b8149c3ad6c3c1e14910164cfdd446a1842
suleiman98 <sl.mustapha98@gmail.com>
2021-04-15 08:16:12 +0100

    add menu

--- src/account/Success.jsx
+++ src/account/Success.jsx
@@ -1,39 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Menu from "../helper/Menu";
 
 export default function Success() {
   return (
     <div>
-      <div className="mt-24">
-        <div className="flex justify-center items-center mb-8">
-          <svg
-            className="w-24 h-24 text-gray-500"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z"
-            />
-          </svg>
+      <div className="h-screen">
+        <div className="mt-24">
+          <div className="flex justify-center items-center mb-8">
+            <svg
+              className="w-24 h-24 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z"
+              />
+            </svg>
+          </div>
+          <div className="text-center mb-2">Thank you!</div>
+          <p className="text-center mb-8">
+            Your order has been placed successful
+          </p>
         </div>
-        <div className="text-center mb-2">Thank you!</div>
-        <p className="text-center mb-8">
-          Your order has been placed successful
-        </p>
+        <Link to="/">
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Continue Shopping
+          </button>
+        </Link>
       </div>
-      <Link to="/">
-        <button
-          type="submit"
-          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Continue Shopping
-        </button>
-      </Link>
+      <Menu />
     </div>
   );
 }
